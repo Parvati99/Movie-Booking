5e48cbff8d11d4aaed5673e284d9ede82e0970ae
Parvati99 <parvatisidu@gmail.com>
2024-09-03 21:48:14 +0530

    signin created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -9,22 +9,34 @@ export const testUser= (req, res)=>{
     const {username, email, phone, password} = req.body;
     const salt = 10;
     const hashedPassword = bcryptjs.hashSync(password, salt);
-    const newUser = new User({username, email, phone, password: hashedPassword});
+    const newUser = new User({username, email, phone, password: hashedPassword,});
    try{
-    await newUser.save();
+     await newUser.save();
     //  res.send("User Created Successfully.");
-    res.status(201).json("User created Successfully")
+      res.status(201).json("User created Successfully")
 
     } catch(error){
-        console.log("Signup error" +error);
+        console.log("Sign-up error" + error );
         res.send(error);
     }
   }
 
   export const signin=async(req, res)=>{
     try{
-      res.send("User login Successfully.");
+      const {email, password}= req.body;
+      const validUser= await User.findOne({email});
+      if(!validUser)
+      {
+        return res.status(401).send({ message: "Invalid email." });
+        // res.send("Invalid email Id");
+      }
+      
+      const validPassword= bcryptjs.compareSync(password, validUser.password);
+      if(!validPassword)
+        return res.status(401).send({ message: "Invalid password." });
+
+      res.status(200).send({ message: "User signed in successfully." });
      } catch(error){
-         console.log("Login error" +error);
+      res.status(500).send({ message: "An error occurred during sign-in. Please try again later." + error }); 
      }
    }
